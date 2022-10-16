414400f6812077e8363d06b300b188a82d2777d8
xrh98 <58538861+xrh98@users.noreply.github.com>
2022-10-16 15:44:14 +0800

    Update default.py
    change data dir

--- lib/config/default.py
+++ lib/config/default.py
@@ -50,10 +50,10 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
-_C.DATASET.DATAROOT = '/home/zwt/bdd/bdd100k/images/100k'       # the path of images folder
-_C.DATASET.LABELROOT = '/home/zwt/bdd/bdd100k/labels/100k'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '/home/zwt/bdd/bdd_seg_gt'                # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = '/home/zwt/bdd/bdd_lane_gt'               # the path of ll_seg_annotations folder
+_C.DATASET.DATAROOT = '/mnt/f/DATASET/bdd100k_yolop'     # the path of images folder
+_C.DATASET.LABELROOT = '/mnt/f/DATASET/bdd100k_yolop/det_annotations'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = '/mnt/f/DATASET/bdd100k_yolop/da_seg_annotations'    # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = '/mnt/f/DATASET/bdd100k_yolop/ll_seg_annotations'    # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
